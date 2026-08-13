0c3dc6697c6fde43c41b6b9ae1f121d544d5fd6e
Doshirak51 <kristofoli@list.ru>
2026-08-14 02:40:35 +0300

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // These values are safe to use in a browser. Do not put a Supabase Secret key here.
 window.FAMILY_ASSISTANT_CONFIG = {
   supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseAnonKey: "sb_publishable_4VzNeZ4COh7fI7v5tNvpFg_u4agoGbZ"
 };
